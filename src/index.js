2dc2fae883b47a340110228bb55200cbfc15bdd0
Grant Winman <grant.winman@gmail.com>
2017-12-08 10:52:30 -0700

    Started pluralsight tutorial.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+render(
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
+);
