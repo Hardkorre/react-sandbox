d95d2fb9541ca6934f67c9b31fdd4723698ba362
Grant Winman <grant.winman@gmail.com>
2017-12-10 16:46:27 -0700

    Initial setup of redux following PluralSight tutorial.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 
 import 'babel-polyfill';
 import React from 'react';
+import {Provider} from 'react-redux';
 import {render} from 'react-dom';
+import configureStore from './store/configureStore';
 import {Router, browserHistory} from 'react-router';
 import routes from './components/common/routes';
 import './styles/styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
+const store = configureStore();
+
 render(
-    <Router history={browserHistory} routes={routes} />,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('app')
 );
