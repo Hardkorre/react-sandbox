566520005f2a39e904bcc216d8249c455282ce35
Grant Winman <grant.winman@gmail.com>
2017-12-11 20:00:04 -0700

    Checking in progress setting up async calls with thunk.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import {render} from 'react-dom';
 import configureStore from './store/configureStore';
 import {Router, browserHistory} from 'react-router';
 import routes from './components/common/routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
     <Provider store={store}>
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,11 +1,9 @@
-import * as actionTypes from '../actions/actionTypes';
+import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action){
     switch(action.type){
-        case actionTypes.CREATE_COURSE:
-            return [...state,
-                   Object.assign({}, action.course)
-            ];
+        case types.LOAD_COURSE_SUCCESS:
+            return action.courses;
         
         default: 
             return state;
