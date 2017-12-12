8524e348a6fa3710f636f635bffabbf7f43c26c1
Grant Winman <grant.winman@gmail.com>
2017-12-12 08:53:35 -0700

    Fixed store to work with redux-thunk.  Thanks for the help John Kerski! REDUX dev tools require its own compose function to make it work with middleware ~ John Kerski.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 
 export default function configureStore(initialState){
-    return createStore(
+    console.log("Creating store");
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store = createStore(
         rootReducer,
-        initialState,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), 
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
+        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
     );
+    return store;
 }
